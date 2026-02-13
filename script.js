de508eadea00d08fdd69e93b8880b9e16d904528
guarinox <guarinox@users.noreply.github.com>
2026-02-13 19:36:09 -0300

    Fix confetti z-index

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ yesBtn.addEventListener('click', (e) => {
     // Disparar confeti
     var duration = 15 * 1000;
     var animationEnd = Date.now() + duration;
-    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 999 };
 
     function random(min, max) {
       return Math.random() * (max - min) + min;
